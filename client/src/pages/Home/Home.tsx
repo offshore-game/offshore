cfea73224675a3b5d3fbbf2220b638d37c3e95d5
HPRocket <38041684+HPRocket@users.noreply.github.com>
2022-12-04 20:49:01 -0500

    CSS Modules

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import './Home.module.css'
+import styles from './Home.module.css'
 
 export default function Home() {
     return (
-        <div className="Home-Container">
+        <div className={styles.container}>
             <center><b>Game Home Page</b></center>
         </div>
     )
