9ea758d7ef432f6dbc4f9599cc39fafb18b1de56
HPRocket <38041684+HPRocket@users.noreply.github.com>
2022-12-04 20:45:59 -0500

    Setup React Router + Cleanup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Home.module.css'
+
+export default function Home() {
+    return (
+        <div className="Home-Container">
+            <center><b>Game Home Page</b></center>
+        </div>
+    )
+}
