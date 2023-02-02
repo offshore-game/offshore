cb94fbc081e6ffe41d5c51adbed245069efeff38
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-02-01 22:02:24 -0500

    Button Combination Puzzle Update
    - Added react-icons as a general icon library for the application.
    - Added a /test route for quickly testing components
    
    - Button Combination Functionality and Object Compilation ready; awaiting to insert the API Call.

--- client/src/Puzzles/ButtonCombination/ButtonCombination.tsx
+++ client/src/Puzzles/ButtonCombination/ButtonCombination.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { RxTriangleUp, RxTriangleDown } from 'react-icons/rx'
 import Button from '../../components/Button/Button'
 import styles from './ButtonCombination.module.css'
 
@@ -11,12 +12,31 @@ export default function ButtonCombination(props: { count: number }) {
 
         for (let i = 0; i <= props.count; i++) {
             setButtonElems(entries => [...entries, <div key={i} className={styles.button}>
-                {/* Insert into here an up and down arrow that will change the .innerHTML property of the button */}
-                ^{i}{">"}
+                <RxTriangleUp className={styles.arrow} onClick={() => {
+
+                    // Add one to the number (cap 9)
+                    const element = document.getElementById(`buttonVal${i}`)!
+                    const numVal = Number(element.innerHTML)
+                    if (numVal < 9) {
+                        element.innerHTML = `${numVal + 1}`
+                    }
+
+                }}/>
+                <div id={`buttonVal${i}`}>0</div>
+                <RxTriangleDown className={styles.arrow} onClick={() => {
+
+                    // Remove one from the number (floor 0)
+                    const element = document.getElementById(`buttonVal${i}`)!
+                    const numVal = Number(element.innerHTML)
+                    if (numVal > 0) {
+                        element.innerHTML = `${numVal - 1}`
+                    }
+                    
+
+                }}/>
             
             </div>])
         }
-        console.log(buttonElems)
     }, [])
 
     return (
@@ -24,11 +44,25 @@ export default function ButtonCombination(props: { count: number }) {
 
             <div className={styles.buttonContainer}>
                 
-                { buttonElems } { /* why no worky */ }
+                { buttonElems }
 
             </div>
 
-            <Button text={"Submit"} onClick={() => {}} style={{margin: "15px", minHeight: "45px"}}/>
+            <Button text={"Submit"} onClick={() => {
+
+                const combinationPayload: { [key: number]: string } = {}
+
+                for (let i = 0; i < buttonElems.length; i++) {
+
+                    const element = document.getElementById(`buttonVal${i}`)!
+                    combinationPayload[i] = element.innerHTML
+
+                }
+
+                // (FEATURE) Insert API Call here
+                console.log(combinationPayload)
+
+            }} style={{margin: "15px", minHeight: "45px"}}/>
             
         </div>
     )
