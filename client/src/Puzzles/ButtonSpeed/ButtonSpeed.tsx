633ce01e6dc7b1e510799b13a2dc1650e0e3df19
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-02-17 17:47:38 -0500

    Update ButtonSpeed.tsx
    this kind of works but i dont like it as a solution anyways

--- client/src/Puzzles/ButtonSpeed/ButtonSpeed.tsx
+++ client/src/Puzzles/ButtonSpeed/ButtonSpeed.tsx
@@ -4,6 +4,9 @@ import styles from './ButtonSpeed.module.css'
 export default function ButtonSpeed(props: { layout: { rows: number, columns: number } }) {
 
     const [ buttons, setButtons ] = useState([] as any[])
+
+    const [ resetTrigger, setResetTrigger ] = useState(false)
+
     const buttonGrid = useRef(undefined as any) as React.MutableRefObject<HTMLDivElement>
 
     type buttonSpeedPayloadType = {
@@ -15,7 +18,11 @@ export default function ButtonSpeed(props: { layout: { rows: number, columns: nu
 
         timing: {
             [key: string]: number[]
-        }
+        },
+
+        gameDuration: number,
+
+        timeToHit: number,
 
     }
 
@@ -26,7 +33,11 @@ export default function ButtonSpeed(props: { layout: { rows: number, columns: nu
         timing: {
             0: [ 2, 5, 10 ], // At 2, 5, and 10 seconds after start
             1: [ 3, 6, 11 ]
-        }
+        },
+
+        gameDuration: 13, // The time the game goes on
+
+        timeToHit: 2,
 
     }
 
@@ -41,6 +52,7 @@ export default function ButtonSpeed(props: { layout: { rows: number, columns: nu
 
     // Render all the buttons
     useEffect(() => {
+        setButtons([])
 
         const buttonCount = props.layout.rows * props.layout.columns
 
@@ -55,41 +67,78 @@ export default function ButtonSpeed(props: { layout: { rows: number, columns: nu
         root.style.setProperty('--buttonSpeed-Columns', `${props.layout.columns}`)
 
 
-    }, [])
+    }, [resetTrigger])
 
     useEffect(() => {
 
         // Lifecycle for the random buttons
-        const startTimestampSec = new Date().getTime() * 1000 // Milliseconds to Seconds
+        const timeouts: any = []
 
         for (const [button, times] of Object.entries(buttonSpeedPayload.timing)) {
-            console.log('1')
             for (const time of times) {
                 // For each time
                 
 
-                setTimeout(() => {
-                    console.log('timeout run')
-
+                let mainTimeout = setTimeout(() => {
                     const element = document.getElementById(`buttonSpeed-${button}`)
-                    console.log(element)
-                    element?.style.setProperty('background-color', 'red') // not working bruh
+                    
+                    element?.style.setProperty('background-color', 'blue') // not working bruh
 
-                    setTimeout(() => {
+                    // TO-DO:
+                    // Listen for a click, if the click does not come within x amount of time, trigger a reset
+
+                    let pass = false
+
+                    element?.addEventListener("mousedown", () => {
+
+                        pass = true // Player clicked button
+
+                        element?.style.setProperty('background-color', 'gray')
+
+                    })
+
+
+                    // Listen for if the player clicked or not
+                    let expirationTimeout = setTimeout(() => {
+
+                        if (!pass) {
+
+                            // Player failed
+                            console.log("fail!")
+                            for (const timeout of timeouts) { console.log('kill timeout'); clearTimeout(timeout) }
+
+                            //for (const btn of buttons) { console.log(btn); btn.style.setProperty('background-color', 'gray') } // DEBUG: why no worky
+
+                            setResetTrigger(resetTrigger ? false : true) // Toggle the trigger
+
+                        }
 
                         // Return to gray after x amount of time
                         element?.style.setProperty('background-color', 'gray')
 
-                    }, 2000 /* 2 seconds */)
+                    }, buttonSpeedPayload.timeToHit * 1000 /* time alloted to click the button */)
+
+                    timeouts.push(expirationTimeout)
 
                 }, time * 1000 /* In milliseconds */)
+
+                timeouts.push(mainTimeout)
 
             }
 
         }
 
+        let gameEndTimeout = setTimeout(() => {
+
+            console.log("game over!")
+
+        }, buttonSpeedPayload.gameDuration * 1000 /* Finish the game after this time */)
+
+        timeouts.push(gameEndTimeout)
         
-    }, [])
+    }, [resetTrigger])
+
+    useEffect(() => { console.log('trigger changed')}, [resetTrigger])
 
     return (
         <div className={styles.container}>
