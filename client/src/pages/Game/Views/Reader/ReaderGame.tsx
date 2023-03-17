4686040912b4bf54c1c4d30a90bb8860bbafdb7b
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-03-17 10:50:18 -0400

    Update ReaderGame.tsx

--- client/src/pages/Game/Views/Reader/ReaderGame.tsx
+++ client/src/pages/Game/Views/Reader/ReaderGame.tsx
@@ -1,48 +1,35 @@
 import React from 'react'
 import { gameInfo, zoneNames } from '../../../../API/requests'
+import ButtonCombinationManual from '../../../../Puzzles/ButtonCombination/Manual/ButtonCombinationManual'
 import { AuthProp } from '../../../../utils/propTypes'
 import ManualTarget from './ManualTarget/ManualTarget'
 import styles from './ReaderGame.module.css'
 
 export default function ReaderGame(props: { gameInfo: gameInfo, setGameInfo: React.Dispatch<gameInfo>, activePuzzle: { element: JSX.Element, zoneName: zoneNames | undefined }, setActivePuzzle: React.Dispatch<{ element: JSX.Element, zoneName: zoneNames | undefined }> } & AuthProp) {
 
-    const puzzleAnswerSamples = []
+    const answerPages = []
     for (const puzzle of props.gameInfo.puzzles) {
         // A solution is provided for this player
         if (puzzle.solution) {
-            puzzleAnswerSamples.push(<ManualTarget key={`${puzzle.zoneName}`} active={true} puzzle={puzzle} setActivePuzzle={props.setActivePuzzle} requests={props.requests}/>)
+
+            if (puzzle.type == "buttonCombination") {
+
+                answerPages.push(<ButtonCombinationManual/>)
+
+            }
+            // Add more puzzles!
         }
 
     }
 
     return (
-        <React.Fragment>
+        <div className={styles.container}>
 
-            { puzzleAnswerSamples }
-
-            <div id="activePuzzleContainer" className={styles.hiddenPuzzle /* hiddenPuzzle, activePuzzle */}>
-
-                <div id="puzzleAnswerOverlay" className={styles.inactiveAnswerOverlay /* inactiveAnswerOverlay, correctAnswerOverlay, incorrectAnswerOverlay */}/>
-
-                <div className={styles.exitCube} onClick={() => {
-                    // Animate the "activePuzzle" div out
-                    const activePuzzleContainer = document.getElementById('activePuzzleContainer')
-                    const shadow = document.getElementById('shadow')
-
-                    if (activePuzzleContainer && shadow) {
-
-                        props.setActivePuzzle({ element: <div/>, zoneName: undefined })
-                        activePuzzleContainer.className = styles.hiddenPuzzle
-                        shadow.style.zIndex = "-1"
-
-                    }
-
-                }}/>
-                { props.activePuzzle.element }
+            <div>
 
             </div>
 
-        </React.Fragment>
+        </div>
     )
 
 }
