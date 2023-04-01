7dcd31ce60255f9efba930f758b9d806bbed86aa
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-01 01:27:43 -0400

    Leaderboard Redesign

--- client/src/components/Leaderboard/Leaderboard.tsx
+++ client/src/components/Leaderboard/Leaderboard.tsx
@@ -1,26 +1,30 @@
 import Entry from './Entry/Entry'
 import styles from './Leaderboard.module.css'
+import { ReactComponent as SolverBackground} from '../../assets/Game/SolverBackground.svg'
+import { ReactComponent as Water} from '../../assets/Game/Water.svg'
+import React from 'react'
 
 export default function Leaderboard(props: { leaderboard: { username: string, coins: number }[] }) {
 
     const currentUsername = localStorage.getItem("username")
 
-    for (const player of props.leaderboard) {
-
-
-
-    }
-
     return (
-        <div className={styles.container}>
-            LEADERBOARD
-            <div className={styles.entries}>
+        <React.Fragment>
+            <SolverBackground className={styles.background}/>
+            <Water className={styles.background}/>
 
-                { props.leaderboard.map(player => <Entry username={player.username} coins={player.coins} isPlayer={ currentUsername == player.username }/>) }
+            <div className={styles.container}>
 
+                <span className={styles.header}>LEADERBOARD</span>
+                <div className={styles.entries}>
+
+                    { props.leaderboard.map(player => <Entry username={player.username} coins={player.coins} isPlayer={ currentUsername == player.username }/>) }
+
+                </div>
+                
             </div>
-            
-        </div>
+        </React.Fragment>
+
     )
 
 }
