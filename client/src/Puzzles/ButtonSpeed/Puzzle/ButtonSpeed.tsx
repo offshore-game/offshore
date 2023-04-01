ca596b2e16a4f3cbb7286aca73b9fdcf69fa5aee
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-01 13:31:57 -0400

    Major Bug Fix
    - Fixed a major bug where spamming the "Start" button on the ButtonSpeed game would break it.

--- client/src/Puzzles/ButtonSpeed/Puzzle/ButtonSpeed.tsx
+++ client/src/Puzzles/ButtonSpeed/Puzzle/ButtonSpeed.tsx
@@ -16,7 +16,7 @@ export default function ButtonSpeed(props: { zoneName: zoneNames, layout: { rows
 
     const timeouts = useRef([] as any[])
 
-    const buttonGrid = useRef(undefined as any) as React.MutableRefObject<HTMLDivElement>
+    const buttonGrid = useRef(undefined as any as HTMLDivElement)
 
     /*
     
@@ -115,6 +115,9 @@ export default function ButtonSpeed(props: { zoneName: zoneNames, layout: { rows
         // Game was told to start
         if (active) {
 
+            // Deactivate the Start button
+            document.getElementById("btnSpeed-controlButton")!.className = styles.disabledControlButton
+
             // Add the buttons
             const totalCount = props.layout.rows * props.layout.columns
         
@@ -177,6 +180,9 @@ export default function ButtonSpeed(props: { zoneName: zoneNames, layout: { rows
                 
                 setButtonsInfo(tempInfo) // Display the state
 
+                // Reactivate the Start button after 2 seconds
+                document.getElementById("btnSpeed-controlButton")!.className = styles.controlButton
+
             }, 2000)
             timeouts.current.push(deactivateTimeout)
 
@@ -197,7 +203,7 @@ export default function ButtonSpeed(props: { zoneName: zoneNames, layout: { rows
 
 
             <div className={styles.controlContainer}>
-                <Button className={active ? styles.disabledControlButton : styles.controlButton} text="Start" onClick={() => {
+                <Button id={"btnSpeed-controlButton"} className={styles.controlButton} text="Start" onClick={() => {
                     
                     // Unpressable while active
                     if (active) return;
