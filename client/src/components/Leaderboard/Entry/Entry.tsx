37bbabfa05554e75638bed3e707d6e798d2fd503
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-02 01:13:45 -0400

    Leaderboard Visuals
    - Ordered the leaderboard from top to bottom
    - Added a count-up animation to the leaderboard

--- client/src/components/Leaderboard/Entry/Entry.tsx
+++ client/src/components/Leaderboard/Entry/Entry.tsx
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react'
 import styles from './Entry.module.css'
 import { BiCoinStack } from 'react-icons/bi'
 
+const sleep = async (ms: number) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 export default function Entry(props: { username: string, coins: number, isPlayer?: boolean }) {
+
+    const [ displayedCoins, setDisplayedCoins ] = useState(0)
+    const [ countUpTimer, setCountUpTimer ] = useState(undefined as any)
+
+    useEffect(() => {
+
+        setCountUpTimer(setInterval(() => {
+
+            setDisplayedCoins(value => value + 1)
+
+        }, 10))
+
+    }, [])
+
+    useEffect(() => {
+
+        if (displayedCoins >= props.coins) {
+
+            clearInterval(countUpTimer)
+            setCountUpTimer(undefined)
+
+        }
+
+    }, [displayedCoins])
 
     return (
         <div className={props.isPlayer ? styles.currentPlayerContainer : styles.container}>
 
-            <span style={{ fontWeight: "500", marginRight: "1%" }}>{props.username}</span> | {props.coins} <BiCoinStack className={styles.coinIcon}/>
+            <span style={{ fontWeight: "500", marginRight: "1%" }}>{props.username}</span> | {displayedCoins} <BiCoinStack className={styles.coinIcon}/>
 
         </div>
     )
--- client/src/components/Leaderboard/Leaderboard.tsx
+++ client/src/components/Leaderboard/Leaderboard.tsx
@@ -2,11 +2,27 @@ import Entry from './Entry/Entry'
 import styles from './Leaderboard.module.css'
 import { ReactComponent as SolverBackground} from '../../assets/Game/SolverBackground.svg'
 import { ReactComponent as Water} from '../../assets/Game/Water.svg'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Leaderboard(props: { leaderboard: { username: string, coins: number }[] }) {
 
     const currentUsername = sessionStorage.getItem("username")
+    const [ sortedLeaderboard, setSortedLeaderboard ] = useState([] as { username: string, coins: number }[])
+
+    useEffect(() => {
+
+        const newLeaderboard = [...props.leaderboard]
+
+        // Sort the leaderboard
+        newLeaderboard.sort((a, b) => {
+
+            return b.coins - a.coins
+
+        })
+
+        setSortedLeaderboard(newLeaderboard)
+
+    }, [])
 
     return (
         <React.Fragment>
@@ -18,7 +34,7 @@ export default function Leaderboard(props: { leaderboard: { username: string, co
                 <span className={styles.header}>Leaderboard</span>
                 <div className={styles.entries}>
 
-                    { props.leaderboard.map(player => <Entry username={player.username} coins={player.coins} isPlayer={ currentUsername == player.username }/>) }
+                    { sortedLeaderboard.map(player => <Entry username={player.username} coins={player.coins} isPlayer={ currentUsername == player.username }/>) }
 
                 </div>
                 
