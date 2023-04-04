2722549c00e531b0c0c6f4eab3e9e16b0690d7eb
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-04 19:03:02 -0400

    SVG Overlap Error

--- client/src/pages/Home/Main/Main.tsx
+++ client/src/pages/Home/Main/Main.tsx
@@ -3,8 +3,9 @@ import { redirect, useNavigate } from "react-router-dom";
 import Button from "../../../components/Button/Button";
 import Home from "../Home";
 import styles from './Main.module.css'
-import { ReactComponent as RedRaft } from '../../../assets/Home/Red Raft.svg'
-import { ReactComponent as BlueRaft } from '../../../assets/Home/Blue Raft.svg'
+import { ReactComponent as RedRaft } from '../../../assets/Home/Red Raft.svg';
+import { ReactComponent as BlueRaft } from '../../../assets/Home/Blue Raft.svg';
+import { ReactComponent as Plank } from '../../../assets/Home/Plank.svg';
 
 export default function Main() {
     const nav = useNavigate()
@@ -26,14 +27,22 @@ export default function Main() {
     return (
         <Home className={styles.container}>
             
-            <div className={styles.raftWrapper}>
-                <RedRaft className={styles.raft}/>
-                <span className={styles.text}>Raft!</span>
-            </div>
+            <div className={styles.raftsContainer}>
+                <div className={styles.raftWrapper}>
+                    <RedRaft className={styles.raft}/>
+                    <span className={styles.text}>Join</span>
+                </div>
 
-            <div className={styles.raftWrapper}>
-                <RedRaft className={styles.raft}/>
-                <span className={styles.text}>Raft!</span>
+                <div className={styles.raftWrapper}>
+                    <BlueRaft className={styles.raft}/>
+                    <span className={styles.text}>Create</span>
+                </div>
+
+            </div>
+            
+            <div className={styles.plankWrapper}>
+                <Plank />
+                <span>How to Play</span>
             </div>
 
         </Home>
