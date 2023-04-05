0cff8f61ba25acd774ff1ea1eabce1de0898528e
HPRocket <38041684+HPRocket@users.noreply.github.com>
2023-04-04 20:16:21 -0400

    Click Sounds (Main)

--- client/src/pages/Home/Main/Main.tsx
+++ client/src/pages/Home/Main/Main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { redirect, useNavigate } from "react-router-dom";
 import Button from "../../../components/Button/Button";
 import Home from "../Home";
@@ -8,27 +8,26 @@ import { ReactComponent as BlueRaft } from '../../../assets/Home/Blue Raft.svg';
 import { ReactComponent as Plank } from '../../../assets/Home/Plank.svg';
 
 export default function Main() {
+
     const nav = useNavigate()
-    /*return (
-        <Home>
-            <Button className={styles.create} text={"Create"} onClick={() => {
-                return nav('/create')
-            }}/>
+    const [ clickUpSound, setClickUpSound ] = useState(undefined as any as HTMLAudioElement)
+    const [ clickDownSound, setClickDownSound ] = useState(undefined as any as HTMLAudioElement)
 
-            <Button className={styles.join} text={"Join"} onClick={() => {
-                return nav('/join')
-            }}/>
+    // Asset Pre-Loader \\
+    useEffect(() => {
 
-            <Button className={styles.howToPlay} text={"How To Play"} onClick={() => {
-                return nav('/tutorial')
-            }}/>
-        </Home>
-    )*/
+        // Load the click sounds
+        setClickUpSound(new Audio('/Sounds/mouse up.mp3'))
+        setClickDownSound(new Audio('/Sounds/mouse down.mp3'))
+
+    }, [])
+
     return (
         <Home className={styles.container}>
             
             <div className={styles.raftsContainer}>
-                <div className={styles.raftWrapper} onClick={() => {
+
+                <div className={styles.raftWrapper} onMouseDown={() => { clickDownSound.play() }} onMouseUp={() => { clickUpSound.play() }} onClick={() => {
 
                     return nav('/join')
 
@@ -37,7 +36,7 @@ export default function Main() {
                     <span className={styles.text}>Join</span>
                 </div>
 
-                <div className={styles.raftWrapper} onClick={() => {
+                <div className={styles.raftWrapper} onMouseDown={() => { clickDownSound.play() }} onMouseUp={() => { clickUpSound.play() }} onClick={() => {
 
                     return nav('/create')
 
@@ -48,7 +47,7 @@ export default function Main() {
 
             </div>
             
-            <div className={styles.raftWrapper} style={{ width: "35%" }} onClick={() => {
+            <div className={styles.raftWrapper} onMouseDown={() => { clickDownSound.play() }} onMouseUp={() => { clickUpSound.play() }} style={{ width: "35%" }} onClick={() => {
 
                 return nav('/tutorial')
 
